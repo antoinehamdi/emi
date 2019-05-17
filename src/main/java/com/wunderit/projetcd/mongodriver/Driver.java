3258a40793c0114870e7f030cbb5c0a88c4b5997
Antoine Hamdi <antoine.hamdi@gmail.com>
2019-05-17 14:38:43 +0200

    modif fichier java

--- src/main/java/com/wunderit/projetcd/mongodriver/Driver.java
+++ src/main/java/com/wunderit/projetcd/mongodriver/Driver.java
@@ -21,10 +21,10 @@ import org.bson.Document;
 public class Driver {
 
     //Environment variables to connect to mongodb database
-    String admin = System.getenv().get("MONGODB_ADMIN_USER");
-    String passwd = System.getenv().get("MONGODB_ADMIN_PWD");
-    String url = System.getenv().get("MONGODB_URL");
-    String port = System.getenv().get("MONGODB_PORT");
+    String admin = System.getenv().get("admin");
+    String passwd = System.getenv().get("admin123");
+    String url = System.getenv().get("192.168.3.47");
+    String port = System.getenv().get("27017");
     
     //Create a connection
     MongoClient mongoClient = MongoClients.create("mongodb://"+admin+":"+passwd+"@"+url+":"+port);
